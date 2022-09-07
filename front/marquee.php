4319a234101d13e610e8255ad16942ad7972d801
FY Kuo <teresa99339@gmail.com>
2022-09-07 19:55:09 +0800

    前台顯示ad 完成

--- front/marquee.php
+++ front/marquee.php
@@ -1,3 +1,9 @@
 <marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
-123
+<?php
+$ads = $Ad->all(['sh'=>1]);
+foreach ($ads as $key => $ad) {
+    echo $ad['text'];
+    echo "&nbsp;/&nbsp;";
+}
+?>
 </marquee>
